b7eca25ffbede589c1adfd75c9f897f426e94145
Geoff Williams <gwilliams@confluent.io>
2023-02-22 21:09:26 +1100

    experiment 1

--- gwilliams_pgsql.tf
+++ gwilliams_pgsql.tf
@@ -1,4 +1,20 @@
-resource "confluent_kafka_cluster" "gwilliams" {
+variable "gwilliams_postgres_url" {
+  description = "SQL DB Connection String"
+  type        = string
+  sensitive   = true
+}
+
+provider "sql" {
+  alias = "gwilliams_sql"
+  url = var.gwilliams_postgres_url
+}
+
+data "sql_query" "gwilliams_sql_topics" {
+  query = "select name from topics"
+}
+
+
+resource "confluent_kafka_cluster" "gwilliams-cluster" {
   display_name = "gwilliams-pgsql-test"
   availability = "SINGLE_ZONE"
   cloud        = "AWS"
@@ -13,3 +29,19 @@ resource "confluent_kafka_cluster" "gwilliams" {
     prevent_destroy = true
   }
 }
+
+
+resource "confluent_kafka_topic" "gwilliams-topics" {
+  kafka_cluster {
+    id = confluent_kafka_cluster.gwilliams-cluster.id
+  }
+
+  for_each = data.sql_query.gwilliams_sql_topics.result
+
+  topic_name = each.value.name
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
